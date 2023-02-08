fc0199e1075a79169c4cfc53dc85425ddcefa0d4
SBiscBoi <wgriffingreen@yahoo.com>
2023-02-07 20:45:24 -0600

    first commit

---
+++ wordfinder.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    //array of required letters
+    char letters[10] = {'v', 'a', 'l', 'e', 'n', 't', 'i', 'n', 'e', '\0'};
+    
+    //find letters in file
+    //remove unexceptable words from file
+    //terminate
+}
